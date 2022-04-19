c760b7ca960f9da795c855aee9c844ebbd029278
Moringa School <karanjasam210@gmail.com>
2022-04-19 11:26:59 +0300

    add constructor to create new quote instances

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from 'src/app/model/quote';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  newQuote = new Quote(0, '', '', '', 0, 0, new Date())
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitGoal() {
+    this.addQuote.emit(this.newQuote);
+
+    //Reset Form
+    this.newQuote = new Quote(0, '', '', '', 0, 0, new Date())
+  }
   constructor() { }
 
   ngOnInit(): void {
