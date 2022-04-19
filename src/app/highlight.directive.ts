86e6a5a151aae4b8f792f6f4d5dc90e6113c0eab
Moringa School <karanjasam210@gmail.com>
2022-04-19 11:44:29 +0300

    add directive backgroung color style

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,6 +5,8 @@ import { Directive } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { 
+     this.elem.nativeElement.style.backgroundColor = '#fff';
+}
 
 }
